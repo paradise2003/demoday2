5c855161b74a50322681046281e929072d4dc480
Vlada <73403094+vladatopada@users.noreply.github.com>
2020-11-24 18:19:16 +0200

    added ex4

---
+++ javaScript-basics/ex.4/ex4.js
@@ -0,0 +1,40 @@
+//Calculate Tip
+function calculateTip() {
+  var costul = document.getElementById("costul").value;
+  var qualify = document.getElementById("qualify").value;
+  var numOfPeople = document.getElementById("persons").value;
+
+  //validate input
+  if (costul === "" || qualify == 0) {
+    alert("Please enter values");
+    return;
+  }
+  //Check to see if this input is empty or less than or equal to 1
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //Calculate tip
+  var total = (costul * qualify) / numOfPeople;
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Display the tip
+  document.getElementById("suma").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+}
+
+//Hide the tip amount on load
+document.getElementById("suma").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
